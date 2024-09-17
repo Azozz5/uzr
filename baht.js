b64b945d17b61369b22a5b78bf12c4213f43fa6f
Azozz5 <161593795+Azozz5@users.noreply.github.com>
2024-09-17 23:01:13 +0300

    baht.js

--- baht.js
+++ baht.js
@@ -100,19 +100,9 @@ var l_itvaiah=setInterval(function(){
     if(myid!=null){
         clearInterval(l_itvaiah);
         $("#users .uzr:contains('ㅤㅤبَاهت ، شُعٍوُرٍㅤㅤㅤㅤㅤㅤㅤ')").addClass('baht1');
-	$("#users .uzr.baht1 span.u-topic").after('<img id="mystory1" class="My_vedio1" title="ستوري" style="float: right;width: 20px;margin-top: 0px;margin-right: 5px;cursor: pointer;" src="https://up6.cc/2024/09/172657472592011.gif">');
-       $("#users .uzr.baht1 u-ico").after('<img id="azoz" class="al-hasni"src="https://up6.cc/2024/09/17265621422271.gif">');
-    
+	       $("#users .uzr.baht1 u-ico").after('<img id="azoz" class="al-hasni"src="https://up6.cc/2024/09/17265621422271.gif">');
 	    $("#users .uzr.baht1 span.uhash, #users .uzr.baht1 img.co").hide(); 
-        $("#upro").before('<div id="movon1" onclick="$(this).hide().html(``);" class="moveon1" style="display:none;width:100%;height:100%;z-index:999999;position: fixed;left: 0px;top: 0px;background-color: rgba(0, 0, 0, 0.6);">');
-        $("div#users .uzr.aiah1 .My_vedio1").click(function(){
-        $("#movon1").show();
-        $('#movon1').addClass('athb1');
-        $("#movon1").before('<div class="modal-dialog fr break" style="height: 338px; max-width: 340px; min-width: 300px; min-height: 599px;overflow: visible;margin-top:40px;"></div>');
-        $("#movon1").append('<div style="max-width: 340px; min-width: 300px; color:white; width: 338px; float: right;" class="modal-header label-primary"><span class="pull-right clickable badge"><i class="fa fa-times"></i></span><label style="margin:1px; float: right;" > My vedio </label></div>');  
-        $("#movon1").append('<iframe id="ifr_mov1" style="max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;" class="modal in" role="dialog" style="z-index: 2100; display: none; style="max-width:240px;" src="https://www.youtube.com/embed/UKRcJ19LW0A" allowfullscreen="" width="338" height="490" frameborder="0"></iframe>');
-        $("#ifr_mov1").show(function(){
-        $("#upro").modal("hide");
+                $("#upro").modal("hide");
     } ); 
 });
     }
